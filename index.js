25e5bfc512a8b5d5f70f262dbe0c405f1f393101
Samy Fadel <samyfadel@google.com>
2022-06-18 16:55:45 +0200

    samy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 9000;
+const port = 80;
 app.get('/', (req, res) => {
     res.send('<h1>Hello World from Docker and Node.js!</h1>');
 });
